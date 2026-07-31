4a17b36267a6f6f9ccaa07a09a2e5ace6aef9733
Mohsin Turabi <137402905+mohsinturabi@users.noreply.github.com>
2026-07-31 15:33:09 +0530

    Create page.tsx

---
+++ app/dashboard/kitchen/page.tsx
@@ -0,0 +1,10 @@
+import RequireKitchen from '@/components/dashboard/RequireKitchen';
+import KitchenView from '@/components/dashboard/KitchenView';
+
+export default function KitchenPage() {
+  return (
+    <RequireKitchen>
+      <KitchenView />
+    </RequireKitchen>
+  );
+}
