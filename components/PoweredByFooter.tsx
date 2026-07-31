a6981bada7d786b49cca7a058c99620b15f7b4ed
Mohsin Turabi <137402905+mohsinturabi@users.noreply.github.com>
2026-07-31 23:46:06 +0530

    Create PoweredByFooter.tsx

---
+++ components/PoweredByFooter.tsx
@@ -0,0 +1,10 @@
+export default function PoweredByFooter() {
+  return (
+    <div className="text-center text-xs text-gray-400 py-4">
+      Powered by{" "}
+      <a href="https://turabilabs.in" target="_blank" className="font-medium text-gray-500 hover:text-orange-500">
+        Turabi Labs
+      </a>
+    </div>
+  );
+}
